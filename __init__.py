836a754e508212b115ad792f4d372700f33c3936
TsuMakoto <tsukamoto@fusic.co.jp>
2023-01-15 19:34:40 +0900

    add root/__init__.py

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from pipeline import Pipeline
+from pipeline.input import Input
+from pipeline.output import Output
+from pipeline.params import Params
+from pipeline.steps.appendix import Appendix
+from pipeline.steps.element import Element
+from pipeline.steps.reader import Reader
+from pipeline.steps.searcher import Searcher
+
+__all__ = [
+    "Pipeline",
+    "Input",
+    "Output",
+    "Params",
+    "Appendix",
+    "Element",
+    "Reader",
+    "Searcher"
+]
