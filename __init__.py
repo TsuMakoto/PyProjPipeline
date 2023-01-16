82d26683c323b587be8a81fe913d7b0b2bd33f95
Tsukamoto <49139020+TsuMakoto@users.noreply.github.com>
2023-01-16 12:06:53 +0900

    __init__.pyの修正

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from pipeline import Pipeline
-from pipeline.input import Input
-from pipeline.output import Output
-from pipeline.params import Params
-from pipeline.steps.appendix import Appendix
-from pipeline.steps.element import Element
-from pipeline.steps.reader import Reader
-from pipeline.steps.searcher import Searcher
+from .pipeline import Pipeline
+from .pipeline.input import Input
+from .pipeline.output import Output
+from .pipeline.params import Params
+from .pipeline.steps.appendix import Appendix
+from .pipeline.steps.element import Element
+from .pipeline.steps.reader import Reader
+from .pipeline.steps.searcher import Searcher
 
 __all__ = [
     "Pipeline",
